9614f5d58783fdc42dcda75ba1e1e97c09232c2d
JULIANO PARNOFF DE ALVARENGA <julianoalvarenga626@gmail.com>
2024-11-06 21:04:33 -0300

    Add files via upload

---
+++ juguigo.java
@@ -0,0 +1,28 @@
+public class Carro {
+        
+private String modelo;
+
+private int qntPorta;
+
+private String placa;
+
+public String getModelo() {
+return modelo;
+}
+public void setModelo(String modelo) {
+this.modelo = modelo;
+}
+public int getQntPorta() {
+    return qntPorta;
+}
+public void setQntPorta(int qntPorta) {
+    this.qntPorta = qntPorta;
+}
+ public String getPlaca(){
+    return placa;
+}
+
+public void setPlaca(String placa) {
+this.placa = placa;
+}
+}
